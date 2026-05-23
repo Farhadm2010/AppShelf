8cad544863e9ed600ec4ba20c0baeb2c95433223
Farhad Mohammadi <farhad.moh31@gmail.com>
2026-05-23 23:11:22 +0700

    Initial Commit

---
+++ AppShelf/AppShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppShelfApp.swift
+//  AppShelf
+//
+//  Created by Farhad-Macbook Air on 2026/5/23.
+//
+
+import SwiftUI
+
+@main
+struct AppShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
